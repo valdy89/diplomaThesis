b9cbb64b78529c3a8c0d22d70d61269b643e2fdd
valusek <valusek@PDC-VALUSEK.pragodata.local>
2015-11-29 19:08:53 +0100

    Moodle

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'wm42.wedos.net';
+$CFG->dbname    = 'd10288_moodle';
+$CFG->dbuser    = 'a10288_moodle';
+$CFG->dbpass    = 'Pikwika1+';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://fryval.cz';
+$CFG->dataroot  = '/data/web/virtuals/10288/virtual/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
