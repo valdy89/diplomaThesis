46d1c1d6bb2a536cfec97d62dc00927ea5860040
valusek <valusek@PDC-VALUSEK.pragodata.local>
2015-12-05 11:32:40 +0100

    First commit mapleta module

---
+++ mod/mapleta/lang/en/mapleta.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for mapleta
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_mapleta
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Maple T.A.';
+$string['modulenameplural'] = 'Maple T.A.s';
+$string['modulename_help'] = 'Use the mapleta module for... | The mapleta module allows...';
+$string['mapletafieldset'] = 'Custom example fieldset';
+$string['mapletaname'] = 'mapleta name';
+$string['mapletaname_help'] = 'This is the content of the help tooltip associated with the mapletaname field. Markdown syntax is supported.';
+$string['mapleta'] = 'mapleta';
+$string['pluginadministration'] = 'Mapleta administration';
+$string['pluginname'] = 'Maple T.A.';
