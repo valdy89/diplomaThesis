3fddb5e773d90e5f2e4969b0ca51083f4d449f11
valusek <valusek@PDC-VALUSEK.pragodata.local>
2016-01-06 16:37:31 +0100

    another progress

--- local/mapleta/lang/cs/local_mapleta.php
+++ local/mapleta/lang/cs/local_mapleta.php
@@ -1,2 +1,19 @@
 <?php
 $string['pluginname'] = 'Maple T.A. rozšíření local';
+$string['linktitle'] = 'Lokální rozšíření Maple T.A.';
+$string['linkname'] = 'Lokální rozšíření Maple T.A.';
+$string['menulink'] = 'Lokální rozšíření Maple T.A.';
+
+
+$string['heading'] = 'Nabídka lokálního rozšíření Maple T.A.';
+$string['monitorheader'] = 'Maple T.A. - monitor';
+$string['synchro'] = 'Spustit manuální synchronizaci';
+
+$string['synchrodone'] = 'Synchronizace dokončena úspěšně.';
+$string['synchrofail'] = 'Synchronizace se nepovedla. Zkuste to prosím později, případně kontaktujte administrátora.';
+$string['cronlink'] = 'Spustin manuálně CRON úlohu.';
+$string['monitor'] = 'Souhrn všech komponent.';
+$string['maple'] = 'Maple Engine';
+$string['database'] = 'PostgreSQL Databazový Server';
+$string['tomcat'] = 'Tomcat Webový Server';
+$string['xcfb'] = 'Stav X Window system (nebo Xvfb)';
